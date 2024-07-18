d723c71ef7e7fd3f4c1d6de56e5bb55b68e6eb2b
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2024-07-18 15:20:16 +0100

    comment out automatic bashrc export for android install

--- util/installation/android.sh
+++ util/installation/android.sh
@@ -7,12 +7,12 @@ wget $CMDLINETOOLS_URL -O /opt/android-sdk/cmdline-tools/tools.zip
 unzip /opt/android-sdk/cmdline-tools/tools.zip -d /opt/android-sdk/cmdline-tools/
 mv /opt/android-sdk/cmdline-tools/cmdline-tools /opt/android-sdk/cmdline-tools/latest
 rm /opt/android-sdk/cmdline-tools/tools.zip
-if ! [[ $(which sdkmanager) ]]; then
-  echo 'export PATH=$PATH:/opt/android-sdk/cmdline-tools/latest/bin' >> ~/.bashrc
-  echo 'export ANDROID_HOME=/opt/android-sdk/' >> ~/.bashrc
-  echo 'export ANDROID_SDK_ROOT=/opt/android-sdk/' >> ~/.bashrc
-  source ~/.bashrc
-fi
+#if ! [[ $(which sdkmanager) ]]; then
+#  echo 'export PATH=$PATH:/opt/android-sdk/cmdline-tools/latest/bin' >> ~/.bashrc
+#  echo 'export ANDROID_HOME=/opt/android-sdk/' >> ~/.bashrc
+#  echo 'export ANDROID_SDK_ROOT=/opt/android-sdk/' >> ~/.bashrc
+#  source ~/.bashrc
+#fi
 yes | /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
 
 echo y | /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --install "emulator" "platform-tools"
