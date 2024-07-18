1f6bd0cdf2a270d88451ad7aa4cc302020885b46
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2024-07-18 12:05:02 +0100

    update pre-req install script

---
+++ util/installation/android.sh
@@ -0,0 +1,22 @@
+CMDLINETOOLS_URL=https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+! [[ $(which java) ]] && sudo apt-get -y install openjdk-17-jdk
+! [[ $(which adb) ]] && sudo apt-get -y install android-tools-adb
+sudo mkdir -p /opt/android-sdk/cmdline-tools/
+sudo chown $USER: -R /opt/android-sdk/
+wget $CMDLINETOOLS_URL -O /opt/android-sdk/cmdline-tools/tools.zip
+unzip /opt/android-sdk/cmdline-tools/tools.zip -d /opt/android-sdk/cmdline-tools/
+mv /opt/android-sdk/cmdline-tools/cmdline-tools /opt/android-sdk/cmdline-tools/latest
+rm /opt/android-sdk/cmdline-tools/tools.zip
+if ! [[ $(which sdkmanager) ]]; then
+  echo 'export PATH=$PATH:/opt/android-sdk/cmdline-tools/latest/bin' >> ~/.bashrc
+  echo 'export ANDROID_HOME=/opt/android-sdk/' >> ~/.bashrc
+  echo 'export ANDROID_SDK_ROOT=/opt/android-sdk/' >> ~/.bashrc
+  source ~/.bashrc
+fi
+yes | /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
+
+echo y | /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --install "emulator" "platform-tools"
+
+# need to create the platform folder otherwise the sdk will claim there is no sdk root
+mkdir /opt/android-sdk/platforms
+
