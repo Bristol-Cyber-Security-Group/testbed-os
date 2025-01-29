ed2a32b7271cd295a2c899a52382df54bc156cde
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2025-01-29 21:36:01 +0000

    a working benchmark script

--- util/benchmarking/run_benchmark.sh
+++ util/benchmarking/run_benchmark.sh
@@ -11,10 +11,29 @@
 #       the benchmark, so either run this once to get these downloaded and discard the result or
 #       prep the environment ahead of time
 
+bench_start=$(date +%s%N)
+
+# create a results directory for this script run
+results_folder_name="$(date +'%Y-%m-%d_%H-%M-%S')_results"
+mkdir -p "results/$results_folder_name"
+cd "results/$results_folder_name"
+results_folder=$(pwd)
+#owner=$(id -un)
+cd ../..
+#chown -R $owner:$owner results/
+
 # store the testbed-os folder so we can easily move between examples without having
 # to work out relative paths between tests
 cd deployments
 project_dir=$(pwd)
+
+# make sure the ssh key is the right permissions for the guests
+ssh_key_loc="../../../../kvm-compose/kvm-compose/assets/id_ed25519_testbed_insecure_key"
+# different relative path for chmod
+sudo chmod 600 "../../../kvm-compose/kvm-compose/assets/id_ed25519_testbed_insecure_key"
+
+# ssh opts to prevent previous connections breaking the script
+ssh_opts="-o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 # we need to launch the testbed server, run it in the background then stop it a the end of the script
 # to run it, we need to be in it's proper run folder
@@ -87,19 +106,81 @@ clones_up_elapsed=$(test_up "bench_clones")
 sleep 1
 clones_down_elapsed=$(test_down "bench_clones")
 
+### Performance timings
 
-# print timings
-echo "== Deploy Timings"
-echo "time for AVD example to deploy: $avd_up_elapsed ms"
-echo "time for AVD example to destroy: $avd_down_elapsed ms"
-echo "time for Docker example to deploy: $docker_up_elapsed ms"
-echo "time for Docker example to destroy: $docker_down_elapsed ms"
-echo "time for Libvirt example to deploy: $libvirt_up_elapsed ms"
-echo "time for Libvirt example to destroy: $libvirt_down_elapsed ms"
-echo "time for Clones example to deploy: $clones_up_elapsed ms"
-echo "time for Clones example to destroy: $clones_down_elapsed ms"
+## CPU
+
+echo "Measuring example CPU/IO/Network stress test times"
+
+# Libvirt one VM example
+echo "Starting Libvirt one VM example ..."
+
+cd "$project_dir/bench_libvirt"
+kvm-compose up > /dev/null 2>&1
+sleep 20
+
+ssh -i $ssh_key_loc nocloud@172.16.1.10 $ssh_opts 'curl -sL https://yabs.sh | bash -s -- -w bench.json -j -s "-"'
+ssh -i $ssh_key_loc nocloud@172.16.1.10 $ssh_opts 'cat bench.json' > $results_folder/one_vm_bench.json
+
+kvm-compose down > /dev/null 2>&1
+cd ..
+
+# Libvirt three VM example
+
+echo "Starting Libvirt three VM example ..."
+
+cd "$project_dir/bench_three_libvirt"
+kvm-compose up > /dev/null 2>&1
+sleep 30
+
+# run benchmark on three VMs, let the final one block
+ssh -i $ssh_key_loc nocloud@172.16.1.11 $ssh_opts 'curl -sL https://yabs.sh | bash -s -- -w bench.json -j -s "-"' > /dev/null 2>&1 &
+ssh -i $ssh_key_loc nocloud@172.16.1.12 $ssh_opts 'curl -sL https://yabs.sh | bash -s -- -w bench.json -j -s "-"' > /dev/null 2>&1 &
+ssh -i $ssh_key_loc nocloud@172.16.1.13 $ssh_opts 'curl -sL https://yabs.sh | bash -s -- -w bench.json -j -s "-"'
+
+# retrieve the results
+echo "retrieving benchmark results from the three VMs ..."
+until ssh -i $ssh_key_loc nocloud@172.16.1.11 -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "[ -f \"bench.json\" ]" > /dev/null 2>&1; do
+  echo "benchmark on VM 1 not yet ready, sleeping 10s"
+  sleep 10
+done
+ssh -i $ssh_key_loc nocloud@172.16.1.11 $ssh_opts 'cat bench.json' > $results_folder/three_vm1_bench.json
+
+until ssh -i $ssh_key_loc nocloud@172.16.1.12 -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "[ -f \"bench.json\" ]" > /dev/null 2>&1; do
+  echo "benchmark on VM 2 not yet ready, sleeping 10s"
+  sleep 10
+done
+ssh -i $ssh_key_loc nocloud@172.16.1.12 $ssh_opts 'cat bench.json' > $results_folder/three_vm2_bench.json
+
+until ssh -i $ssh_key_loc nocloud@172.16.1.13 -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "[ -f \"bench.json\" ]" > /dev/null 2>&1; do
+  echo "benchmark on VM 3 not yet ready, sleeping 10s"
+  sleep 10
+done
+ssh -i $ssh_key_loc nocloud@172.16.1.13 $ssh_opts 'cat bench.json' > $results_folder/three_vm3_bench.json
+
+
+# done with tests
+echo "benchmark suite finished, wrapping up ..."
 
 # finally stop the testbed server
 echo "stopping the testbed server"
 kill $testbed_server_pid
 wait $testbed_server_pid 2>/dev/null
+
+# save results
+cd "$project_dir/../results/$results_folder_name"
+
+echo "saving up and down timing results into up.csv and down.csv, these are in milliseconds"
+echo "avd,docker,libvirt,clones" > up.csv
+echo "$avd_up_elapsed,$docker_up_elapsed,$libvirt_up_elapsed,$clones_up_elapsed," >> up.csv
+echo "avd,docker,libvirt,clones" > down.csv
+echo "$avd_down_elapsed,$docker_down_elapsed,$libvirt_down_elapsed,$clones_down_elapsed," >> down.csv
+
+echo "benchmark results json files have been saved, the filename represents the test"
+
+# give time of full suite
+bench_end=$(date +%s%N)
+bench_elapsed=$(( bench_end - bench_start ))
+echo "the whole benchmark script took $(awk "BEGIN {print $bench_elapsed / 1000000000}")s to run"
+
+echo "benchmarking done."
