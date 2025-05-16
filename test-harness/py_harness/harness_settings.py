3c79ecae7cf07ef958d3c17228bd2848e8a849f4
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2025-05-16 08:39:30 +0100

    harness settings gets commit hash for workflows

--- test-harness/py_harness/harness_settings.py
+++ test-harness/py_harness/harness_settings.py
@@ -1,8 +1,9 @@
 import logging
+import os
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # harness settings
-test_id = "dev" # TODO set to commit hash from ENV if present, default to dev
+test_id = os.getenv('COMMIT_HASH', 'dev')
 workspace = f"/var/lib/libvirt/images/testbed-os/test-harness/{test_id}"
 max_n_hosts = 1  # how many hosts the test harness will go up to before stopping, max 3 supported
 
