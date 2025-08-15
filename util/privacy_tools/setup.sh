65d1816df91e3f68468aa154fddd423bb679aa50
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2025-08-15 11:12:05 +0100

    update the installation and version of the privacy tools used in the testbed

--- util/privacy_tools/setup.sh
+++ util/privacy_tools/setup.sh
@@ -1,4 +1,4 @@
-RELEASE_VERSION=v1.0.2
+RELEASE_VERSION=27d98fab34c17e7537b539f6bc5fb0dd84f0fbbb
 
 echo "installing Frida-Tools repo"
 cd /var/lib/testbedos/tools/
@@ -10,21 +10,19 @@ if [ -d "Frida-Tools" ]; then
     sudo git pull
 else
     echo "Frida Tools does not exist, pulling from GitHub"
-    sudo git clone -b $RELEASE_VERSION https://github.com/Bristol-Cyber-Security-Group/Frida-Tools.git
+    sudo git clone https://github.com/Bristol-Cyber-Security-Group/Frida-Tools.git
     cd Frida-Tools || exit
+    sudo git checkout $RELEASE_VERSION || exit
 fi
 
 # set up a pip environment just for the frida tools
 FRIDA_VENV=/var/lib/testbedos/tools/frida_tools_venv
 sudo mkdir -p $FRIDA_VENV
 sudo chmod 777 $FRIDA_VENV
-python3 -m venv $FRIDA_VENV
-$FRIDA_VENV/bin/pip install -U pip setuptools
-$FRIDA_VENV/bin/pip install poetry
+sudo python3 -m venv $FRIDA_VENV
+sudo $FRIDA_VENV/bin/pip install -U pip setuptools
 
-# set up the python environment for the Frida-Tools
-# need to ignore keyring https://github.com/python-poetry/poetry/issues/8623
-sudo PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring $FRIDA_VENV/bin/poetry install
+sudo $FRIDA_VENV/bin/pip install -r requirements.txt
 
 # update the python path in test privacy script to the environment created just now
-sudo sed -i 's#PYTHON="/path/to/poetry/env/bin/python"#PYTHON="/var/lib/testbedos/tools/frida_tools_venv/bin/poetry run python"#g' /var/lib/testbedos/tools/Frida-Tools/test-privacy.sh
+sudo sed -i 's#PYTHON="/path/to/poetry/env/bin/python"#PYTHON="/var/lib/testbedos/tools/frida_tools_venv/bin/python"#g' /var/lib/testbedos/tools/Frida-Tools/test-privacy.sh
