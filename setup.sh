fa19573b804541ff400d278e5dde95850c6ac0a0
JollyJellyBerry <syuleymansuner@gmail.com>
2025-11-10 13:44:27 +0000

    Added basic installer safety for OS version in the helper script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-sudo apt install -y ansible
-cd setup/singleton
-ansible-playbook --ask-become-pass setup.yml
+#!/bin/bash
+export OS_PRETTY_NAME=$(grep '^PRETTY_NAME' /etc/os-release)
+if [[ "$OS_PRETTY_NAME" ==  'PRETTY_NAME="Debian GNU/Linux 12 (bookworm)"' ]]; then
+	sudo apt install -y ansible
+	cd setup/singleton
+	ansible-playbook --ask-become-pass setup.yml
+else
+	echo "The only supported version for TestbedOS is Debian GNU/Linux 12 (bookworm)"
+	exit 1
+fi
