d7db9c83fbcd2232264437a7ca426d67c13ce314
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2025-11-11 17:34:37 +0000

    rename test to be explicit this is a lack of internet test, so the curl commands should fail

--- test-harness/py_harness/test_cases/ovn.py
+++ test-harness/py_harness/test_cases/ovn.py
@@ -41,7 +41,8 @@ class OvnTestCase(TestCase):
 
         # this should be false as this scenario has no public switch to bridge the logical network to the host network,
         # so we will negate the test outcome, as a failed connection is a pass
-        internet_test = check_internet_connectivity(self.test_case_name, self.linked_clone_hosts, merged_guests)
+        internet_test = confirm_no_internet_connectivity(self.test_case_name, self.linked_clone_hosts, merged_guests)
+        print("@@@", internet_test)
         internet_test.success = not internet_test.success
 
         return [
