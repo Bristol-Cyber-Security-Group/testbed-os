ab07b3a60e1bffb76428dfa8371c6d1ce08e81c7
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2025-11-24 15:51:54 +0000

    PTBED-330 - Fix mount test harness test case (#38)
    * pre-define CDROM in guests in case it is needed later, and don't detach CDROM when working with old libvirt versions to work around hotplug issues
    
    * add a wait for cloud-init to finish running to let the context mount to appear
    
    * fix check env var, and update context test to be more specific (not failing)
    
    * fix missing cloud-init context mount by specifying a dependable drive id and forcing a wait for cloud-init to run before continuing
    
    * update the docker env var test
    
    * add relative path correction for docker mounts and env_file and re-enable relevant tests ... pending fixes for cluster mode
    
    * change how docker exec commands accept user commands

--- test-harness/py_harness/test_cases/mount.py
+++ test-harness/py_harness/test_cases/mount.py
@@ -35,39 +35,56 @@ class MountTestCase(TestCase):
             self.linked_clone_hosts,
             "_check_setup_script_artefact_exists",
         )
+        # this is here so that we don't run the tests following before cloud-init finishes, meaning the context folder
+        # might not be available yet for example
+        cloud_init_wait = run_command(
+            "client1",
+            "timeout 300 cloud-init status --wait",
+            self.test_case_name,
+            self.linked_clone_hosts,
+            "_ensure_cloud_init_finished_running",
+        )
+        # the context folder should be made, but also check if a file inside it exists to catch any bugs relating to
+        # zipping the context payload to be mounted into the guest
         context_artefact = run_command(
             "client1",
-            "ls /etc/nocloud/context/",
+            "ls /etc/nocloud/context/context.txt",
             self.test_case_name,
             self.linked_clone_hosts,
             "_check_context_artefact_exists",
         )
-        # env_var = run_command(
-        #     "client1",
-        #     """[ -n "${MOUNT_ENV_TEST}" ] && exit 0 || exit 1""",
-        #     self.test_case_name,
-        #     self.linked_clone_hosts,
-        #     "_check_env_var_exists",
-        # )
+        # this checks against the cloud-init environment tooling, we have not configured this to load permanently as
+        # that would require a VM restart after placing into /etc/environment
+        # ... this string here is a bit weird, needing to work around the limitations of how the kvm-compose exec
+        # command and python escape strings work - you need to send a string so that the $ isn't evaluated before being
+        # sent to the guest
+        env_var = run_command(
+            "client1",
+            r""" "test \$(cloud-init query ds.meta_data.environment.MOUNT_ENV_TEST) = 123" """,
+            self.test_case_name,
+            self.linked_clone_hosts,
+            "_check_env_var_exists",
+        )
 
-        ### docker
-        # env_file_var_docker = run_command(
-        #     "client2",
-        #     """[ -n "${DOCKER_ENV}" ] && exit 0 || exit 1""",
-        #     self.test_case_name,
-        #     self.linked_clone_hosts,
-        #     "_check_env_file_var_exists_docker",
-        # )
-        # env_var_docker = run_command(
-        #     "client2",
-        #     """[ -n "${TWO}" ] && exit 0 || exit 1""",
-        #     self.test_case_name,
-        #     self.linked_clone_hosts,
-        #     "_check_env_var_exists_docker",
-        # )
+        ## docker
+        env_file_var_docker = run_command(
+            "client2",
+            """'test $DOCKER_ENV = success'""",
+            self.test_case_name,
+            self.linked_clone_hosts,
+            "_check_env_file_var_exists_docker",
+        )
+        env_var_docker = run_command(
+            "client2",
+            """'test $TWO = 2'""",
+            self.test_case_name,
+            self.linked_clone_hosts,
+            "_check_env_var_exists_docker",
+        )
+        # the contents of ./context/ in the yaml will be placed inside /opt (docker does not preserve the parent folder)
         mount_docker = run_command(
             "client2",
-            "ls /opt/context",
+            "ls /opt/context.txt",
             self.test_case_name,
             self.linked_clone_hosts,
             "_check_mount_exists_docker",
@@ -77,11 +94,12 @@ class MountTestCase(TestCase):
             up_result_report,
             run_script_artefact,
             setup_script_artefact,
+            cloud_init_wait,
             context_artefact,
-            # env_var,
-            # env_file_var_docker,
-            # env_var_docker,
+            env_var,
+            env_file_var_docker,
+            env_var_docker,
             mount_docker,
         ]
 
-# registered_test_cases.append(MountTestCase)
+registered_test_cases.append(MountTestCase)
