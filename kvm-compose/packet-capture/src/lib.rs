e21ab8bbdce5af128067ae1a23de172a0225f6b3
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2025-11-25 11:30:02 +0000

    PoC basic capture in async context

--- kvm-compose/packet-capture/src/lib.rs
+++ kvm-compose/packet-capture/src/lib.rs
@@ -1,21 +1,59 @@
-use pcap::Capture;
+use pcap::{Capture, PacketCodec};
 
-struct Packet {
-    timestamp: u64,
-    src_ip: [u8; 4],
-    dst_ip: [u8; 4],
+use futures::StreamExt;
+
+
+struct RawPacket;
+impl PacketCodec for RawPacket {
+    type Item = String;
+
+    fn decode(&mut self, packet: pcap::Packet<'_>) -> Self::Item {
+        format!("{packet:?}")
+    }
 }
 
-async fn packet_capture(
-
+pub async fn packet_capture(
+    interface: String,
+    stop_rx: tokio::sync::oneshot::Receiver<()>,
 ) -> anyhow::Result<()> {
-    let mut capture = Capture::from_device("vm-ovn00")?
-        .immediate_mode(true)
-        .open()?;
 
-    while let Ok(packet) = capture.next_packet() {
-        println!("{:#?}", packet);
+    // open connection to interface
+    let capture = Capture::from_device(interface.as_str())?
+        .immediate_mode(true)
+        .open()?
+        // set non-blocking so we can check if we need to exit due to a stop instruction, otherwise
+        // the `next_packet` function will block until the next packet before we are allows to check
+        .setnonblock()?;
+
+    // open a packet stream that can be used in futures
+    let stream = capture.stream(RawPacket {})?;
+
+    // this creates a stream, which is an async iterator, the stream creates a future for each
+    // packet that is captured which is then processed inside the closure below
+    let fut = stream.for_each(move |s| {
+        tracing::info!("inside: {s:?}");
+
+        // we need to return an empty future here, but this should be changed to a future that can
+        // be processed into the destination data sink, keep this future light
+        futures::future::ready(())
+    });
+
+    // wait on either the stop instruction being received, or the stream closing itself, so
+    // whichever happens this will idiomatically and safely stop the processing of the packets
+    tokio::select! {
+        _ = stop_rx => {
+            // we received a stop instruction
+            tracing::info!("received stop instruction");
+        }
+        result = fut => {
+            // the stream has closed itself, for some reason
+            tracing::error!("packet capture stream stopped: {:?}", result);
+        }
     }
+
+    tracing::info!("capture complete");
 
     Ok(())
 }
+
+
---
+++ kvm-compose/packet-capture/src/main.rs
@@ -0,0 +1,77 @@
+use clap::Parser;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+use packet_capture::packet_capture;
+
+#[derive(Parser, Debug)]
+struct CliArgs {
+
+    /// The interface or port to capture from
+    #[clap(short, long)]
+    interface: String,
+
+    // TODO - recreate the ovs-tcpdump api
+
+    // db-sock
+
+    // dump-cmd
+
+    // mirror-to
+
+    // span
+
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+
+    // set up logging for the CLI
+    let stdout_log = tracing_subscriber::fmt::layer();
+    tracing_subscriber::registry()
+        .with(stdout_log.with_filter(LevelFilter::INFO))
+        .init();
+
+    // parse the CLI options and then run the packet capture with the given arguments
+    let cli_args = CliArgs::parse();
+    run_loop(cli_args.interface).await?;
+
+    Ok(())
+}
+
+/// In the run loop, we will run the pcap listener in a loop while at the same time waiting for a
+/// stop instruction to gracefully end the packet capture.
+pub async fn run_loop(
+    interface: String,
+) -> anyhow::Result<()> {
+    // channel that will be used to send and listen for the stop instruction
+    let (stop_tx, stop_rx) = tokio::sync::oneshot::channel::<()>();
+
+    // packet capture future start, this wraps the blocking thread call in `packet_capture`
+    let packet_capture_handle = tokio::spawn(async move {
+        packet_capture(interface, stop_rx).await
+    });
+
+    // start future to listen to ctrl+c
+    let stop_handle = tokio::spawn(async move {
+        let _ = tokio::signal::ctrl_c().await;
+        tracing::info!("captured ctrl + C, gracefully stopping command");
+    });
+
+    // wait for either packet capture to error or for ctrl+c to interrupt to exit gracefully
+    tokio::select! {
+        _ = stop_handle => {
+            // ctrl+c triggered, send stop token to packet capture
+            tracing::info!("stopping command");
+            stop_tx.send(())
+                .map_err(|_| anyhow::anyhow!("failed to send stop instruction to channel"))?;
+        }
+        result = packet_capture_handle => {
+            anyhow::bail!("packet_capture_handle exited with {:?}", result);
+        }
+    }
+
+    Ok(())
+}
