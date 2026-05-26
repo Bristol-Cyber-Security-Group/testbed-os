2ef543f4866b366226bd3a1500f22dd5bcef1b2a
drrtdrrt <9076599+drrtdrrt@users.noreply.github.com>
2026-05-26 14:11:55 +0100

    add test harness tests for state rework

--- test-harness/py_harness/config/guest_control.py
+++ test-harness/py_harness/config/guest_control.py
@@ -2,13 +2,15 @@ import subprocess
 from subprocess import Popen
 
 
-def ssh_command(cmd: str, ssh_key: str, hostname: str) -> subprocess.CompletedProcess:
+def ssh_command(cmd: str, ssh_key: str, hostname: str, capture_output: bool = False) -> subprocess.CompletedProcess:
     return subprocess.run(["ssh", "-i", ssh_key,
                           "-o", "StrictHostKeyChecking no",
                           "-o", "UserKnownHostsFile /dev/null",
                           hostname,
                           cmd,
-                          ])
+                          ],
+                          capture_output=capture_output,
+                          )
 
 def long_running_ssh_command(cmd: str, ssh_key: str, hostname: str) -> Popen[str]:
     return subprocess.Popen(["ssh", "-i", ssh_key,
